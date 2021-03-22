fa7a0062b686ea88bd8e2fb68e8f60cd2cecf266
Matthew <21284075+MattieX@users.noreply.github.com>
2021-03-22 20:52:19 +0000

    Add further error handling and improve variable names

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Snapchat Memories Downloader
- Provides a method to parse Memories, download them and rename the files from a Snapchat Personal Data Request
+***
+## What does this script do?
+The purpose of this Python Script is to download your snapchat memories provided by a Data Request and appropriately rename them, utilising date metadata if it's detected.
+
+## Why?
+By default, Snapchat provide you with a JSON File of your memories and not the files themselves, these URL's require a post request in order to actually obtain the download link.
+If you were to do this manually, this would take a significant amount of time, prompting me to create this script.
+
+## How to run it?
+You need ```Python``` and ```Pip``` installed on your system.
+
+- Git Clone this repository
+- Make a snapchat data request by following the guide here: https://support.snapchat.com/en-GB/a/download-my-data
+- Extract the ```mydata_<date>.zip``` and place the ```memories_history.json``` file in the Git Repo folder
+- Open a Terminal that has Python in the PATH and cd into the Git Repo
+- Run ```pip install ffmpeg``` - https://pypi.org/project/ffmpeg/
+- Before continuing, verify the following items are in your Current Working Directory:
+  - `download_memories.py` - File
+  - `Download` - Folder (Empty!)
+  - `memories_history.json`
+- Finally, run ```python download_memories.py``` - File (Found in a Snapchat Data Request)
+- Your Memories will be downloaded in the ```Download``` folder in your current working directory.
+
+#### Please note, this script assumes that you have an empty ```Download``` folder as per the Repo before running
+
+## Any issues?
+Please feel free to raise any issues or enhancements you think would be beneifical for this script in the issues tab
--- download_memories.py
+++ download_memories.py
@@ -5,8 +5,9 @@ import threading
 import time
 import ffmpeg
 
-
+"""Renames the Memories using Metadata if found"""
 def rename_files():
+    # Download Folder Name
     dl_path = os.path.join(os.getcwd(), 'Download')
     os.chdir(dl_path)
     # Initialises a Count for Files that do not have date metadata
@@ -20,20 +21,21 @@ def rename_files():
                 vid_exif = ffmpeg.probe(originalFilename)
                 # Uses FFMPEG Package Logic in order to get the correct time
                 creation_time = vid_exif['streams'][0]['tags']['creation_time']
-                destination_filename = "VID_" + str(creation_time).partition('T')[0]
-                print(destination_filename)
+                destination_filename = "VID_" + str(creation_time).partition('T')[0] + '.mp4'
+                print(f'Dest_Filename: {destination_filename}')
                 os.rename(originalFilename, destination_filename)
             except:
                 count += 1
-                print(f"No Creation Date was"
-                      f"found for video {originalFilename}!")
                 destination_filename = 'VID_Memory_' + str(count) + ".mp4"
+                print(f'No Creation Date was '
+                      f'found for video {originalFilename}\n'
+                      f'Renaming to: {destination_filename}')
                 os.rename(originalFilename, destination_filename)
         # Snapchat Image Memories do not contain Date Metadata
         else:
             count += 1
             destination_filename = "IMG_Memory_" + str(count) + ".png"
-            print(destination_filename)
+            print(f'Dest_Filename: {destination_filename}')
             os.rename(originalFilename, destination_filename)
 
 
@@ -47,9 +49,14 @@ def download_file(download_link, media_type):
     with requests.get(download_link, stream=True) as memories_link:
         # Raises a HTTP Exception if one occured (Non-200 Response)
         memories_link.raise_for_status()
-        with open(dl_path, 'wb') as f:
-            for chunk in memories_link.iter_content(chunk_size=8192):
-                f.write(chunk)
+        try:
+            with open(dl_path, 'wb') as outputfile:
+                for chunk in memories_link.iter_content(chunk_size=8192):
+                    outputfile.write(chunk)
+        except IOError:
+            print(f'There was an error creating the file: {fileStr} '
+                  f'at the path: {dl_path} '
+                  f'The download link was: {download_link}')
 
 
 """Post Request is made to obtain the Download Links"""
@@ -58,30 +65,38 @@ def makeRequest(url, media_type):
     try:
         response.raise_for_status()
         download_link = response.text
-        t = threading.Thread(target=download_file, args=(download_link, media_type))
+        print(f'Download_URL: {download_link}')
+        t = threading.Thread(target=download_file,
+                             args=(download_link, media_type))
         t.start()
-    except requests.exceptions.HTTPError as error:
-        print(f"There was an error accessing {url}\n"
-              f"The HTTP Error Code returned was: {response.status_code}\n")
+    except requests.exceptions.HTTPError:
+        print(f'There was an error accessing {url}\n'
+              f'The HTTP Error Code returned was: {response.status_code}\n')
 
 
-"""Manages Threaded Processes.
-Gets download links --> Downloads Files --> Renames Files"""
+"""Manages Threaded Processes
+Gets Download links --> Downloads Files --> Renames Files"""
 def main():
-    print("=====Getting Download Links=====")
+    print('=====Getting Download Links & '
+          'Downloading Files=====')
+    time.sleep(2)
     with open('memories_history.json') as json_file:
         data = json.load(json_file)
         for i in range((len(data["Saved Media"]))):
             url = (data["Saved Media"][i]["Download Link"])
             media_type = (data["Saved Media"][i]["Media Type"])
-            print(url)
+            print(f'Query_URL: {url}')
             t = threading.Thread(target=makeRequest, args=(url, media_type))
             t.start()
+            # Threading Delay
             time.sleep(0.2)
 
-    print("=====Finished Downloading Files!=====")
-    print("=====Renaming Files...=====")
+    print('=====Finished Downloading Files!=====')
+    print('=====Renaming Files...=====')
+    time.sleep(2)
     rename_files()
+    print('=====Downloading & Renaming Complete!=====')
+    print('=====Press Control + C To Exit=====')
 
 
 if __name__ == "__main__":
